cf399605118a1d1b9c4bd1ad93de564f3195fd97
Priyanshu <priyanshu9943@gmail.com>
2021-04-14 20:39:34 +0530

    init

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ function appendDiv() {
     mar = mar2;
   }
 
-  ob.src = `C:/Users/priya/OneDrive/Desktop/Piano Tiles (Genisis)/(${
+  ob.src = `/(${
     Math.floor(Math.random() * 9) + 1
   }).jpg`;
 
